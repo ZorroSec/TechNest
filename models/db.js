10818f5d0dca0349faa3da1fac4655cc10fb3763
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 20:37:33 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,8 @@
 import { Sequelize, DataTypes } from "sequelize";
 
-export default sequelize = new Sequelize('technest', 'root', '', {
+const sequelize = new Sequelize('technest', 'root', '', {
     dialect: 'mysql',
     host: 'localhost'
 })
+
+export default sequelize
