35d49a505140a13899d6e1a4185690582dcdfd42
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 12:04:36 -0300

    Update Login.js

--- routes/Login.js
+++ routes/Login.js
@@ -2,5 +2,10 @@ import app from "../config/config.js";
 import connection from "../connection/connection.js";
 
 export default app.get('/login', (req, res)=>{
-    res.render('login')
+    function click__btn__login(){
+        const email = req.body.email
+        const senha = req.body.senha
+        console.log(nome, senha)
+    }
+    res.render('login', { click__btn__login: click__btn__login() })
 })
