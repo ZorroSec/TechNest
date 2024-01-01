a528ea9d3bac2cae3bb29ab1059a2fa165769cca
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 23:26:11 -0300

    Create route /

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -3,11 +3,14 @@ import mysql from "mysql2"
 import connection from "../connection/connection.js";
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser";
-
+import Posts from "../post/post.js";
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 export default app.get('/', (req, res)=>{
-    res.render('home')
+    Posts.findAll().then((data)=>{
+        res.render('hello')
+        console.log(data)
+    })
 })
