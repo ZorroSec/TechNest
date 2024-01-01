d18a0a9fad7592e82e94f73c21bc8dd47eae6ae7
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 23:18:46 -0300

    Update Homepage.js

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -1,5 +1,13 @@
 import app from "../config/config.js";
+import mysql from "mysql2"
+import connection from "../connection/connection.js";
+import { engine } from "express-handlebars"
+import bodyParser from "body-parser";
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 export default app.get('/', (req, res)=>{
-    res.send('hello')
+    res.render('home')
 })
