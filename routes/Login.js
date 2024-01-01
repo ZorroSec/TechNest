9e23069fa4143104e924d7992d431f5b0d0e30dd
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 12:10:26 -0300

    Update Login.js

--- routes/Login.js
+++ routes/Login.js
@@ -1,11 +1,16 @@
 import app from "../config/config.js";
 import connection from "../connection/connection.js";
+import bodyParser from "body-parser";
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-export default app.get('/login', (req, res)=>{
+export default app.route('/login').get((req, res)=>{
+    res.render('login')
+}).post((req, res)=>{
     function click__btn__login(){
         const email = req.body.email
         const senha = req.body.senha
-        console.log(nome, senha)
+        console.log(email, senha)
     }
     res.render('login', { click__btn__login: click__btn__login() })
 })
