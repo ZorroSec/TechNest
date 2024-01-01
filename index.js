4a79ab462175c9b0a1d8373591ff112edba6fd51
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 22:31:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import connection from "./connection/connection.js"
 import express from "express"
 import app from "./config/config.js"
-import routes from "./controllers/routes.js"
+import routes from "./routes/routes.js"
 app.listen(3000)
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 import app from "../config/config.js";
-
+app.get('/test', (req, res)=>{
+    res.send('tests')
+})
 export default app.get('/', (req, res)=>{
     res.send('hello')
 })
