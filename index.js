4a9658d2281d090959f32fe1579192e8c4a2e954
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 23:20:49 -0300

    Listenning the project

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import express from "express"
 import app from "./config/config.js"
 import Homepage from "./routes/Homepage.js"
 import "./routes/Test.js"
-app.listen(3000)
+app.listen(3000, (err)=>{
+    try{
+        console.log({
+            message: "Success"
+        })
+    } catch (err){
+        console.log(err)
+    }
+
+})
