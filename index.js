e34d65d41ecabec6cd23d9527170ad91c4edf5d0
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 09:44:52 -0300

    Indexing a new instance in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import app from "./config/config.js"
 import"./routes/Homepage.js"
 import "./routes/Test.js"
 import "./routes/Add.js"
+app.use(express.static('public'))
 app.listen(3000, (err)=>{
     try{
         console.log({
