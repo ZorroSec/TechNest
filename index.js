2d032d5b3bc3bd785fb91217e8c5c67000ab758f
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-31 22:51:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import connection from "./connection/connection.js"
 import express from "express"
 import app from "./config/config.js"
 import Homepage from "./routes/Homepage.js"
+import "./routes/Test.js"
 app.listen(3000)
