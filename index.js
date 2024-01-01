f132bc920ee796cc7c4caa1b5850bba446a23270
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 12:04:55 -0300

    Configure index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import connection from "./connection/connection.js"
 import express from "express"
 import app from "./config/config.js"
+import bodyParser from "body-parser"
 import"./routes/Homepage.js"
 import "./routes/Test.js"
 import "./routes/Add.js"
 import "./routes/InitialRoute.js"
 import "./routes/Login.js"
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.listen(3000, (err)=>{
     try{
         console.log({
