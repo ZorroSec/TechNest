8ad43b008b52436ca4dac4deea14bb57578c6d1e
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 11:39:29 -0300

    Creating the route /homepage

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -8,7 +8,7 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-export default app.get('/', (req, res)=>{
+export default app.get('/homepage', (req, res)=>{
     connection.query("SELECT * FROM posts", (results, fields)=>{
         res.render('home', { posts: fields })
     })
