9dac4b89070963a266664e49167c164a1f41c91d
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 21:54:45 -0300

    ORDER BY id DESC

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -9,7 +9,7 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 export default app.get('/homepage', (req, res)=>{
-    connection.query("SELECT * FROM posts", (results, fields)=>{
+    connection.query("SELECT * FROM posts ORDER BY id DESC", (results, fields)=>{
         res.render('home', { posts: fields })
     })
 })
