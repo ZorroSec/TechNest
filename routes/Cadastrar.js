13522ca09efd0dd5c884f6d072c3745d5b8e0119
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-02 07:36:33 -0300

    Update Cadastrar.js

--- routes/Cadastrar.js
+++ routes/Cadastrar.js
@@ -5,5 +5,9 @@ import app from "../config/config.js";
 export default app.route('/cadastro').get((req, res)=>{
     res.render('cadastro')
 }).post((req, res)=>{
-
+    function cadastro__btn(){
+        const nome = req.body.nome
+        console.log(nome)
+    }
+    res.render('cadastro', { cadastro__btn: cadastro__btn() })
 })
