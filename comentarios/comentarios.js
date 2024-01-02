d4c766a3bd561b3ea902da25ed2c1e597076c41f
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-02 10:14:57 -0300

    Create model Comentarios in comentarios.js

---
+++ comentarios/comentarios.js
@@ -0,0 +1,23 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../models/db.js";
+
+const Comentarios = sequelize.define("comentarios", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    comentario: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    data: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+})
