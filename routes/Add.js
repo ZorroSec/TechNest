b524fbdc1597061ee93d28c49e6951c2b9ed1cfb
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-01 21:16:53 -0300

    Create a route in add.js and create a template add.handlebars

--- routes/Add.js
+++ routes/Add.js
@@ -10,11 +10,12 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 export default app.get('/add', (req, res)=>{
-    Posts.create({
-        nome: "Zezao",
-        titulo: "Lorem Ispum",
-        post: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        data: Date()
-    })
-    res.send("this is area opf tests")
+    // Posts.create({
+    //     nome: "Zezao",
+    //     titulo: "Lorem Ispum",
+    //     post: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //     data: Date()
+    // })
+    // res.send("this is area opf tests")
+    res.render('add')
 })
